313b421dd9e52afd6f6b2fa63340991ee81949db
vpk79 <someone_bul@yahoo.com>
2024-04-24 15:04:44 +0300

    up

--- my-test-app/src/app/app.component.ts
+++ my-test-app/src/app/app.component.ts
@@ -6,16 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  showSecret: boolean = false;
-  arr: number[] = [];
-  counter: number = 0;
-  blue = {background: 'blue'}
-  white= 'white';
 
-  onToggleDisplay() {
-    this.showSecret = !this.showSecret;
-    this.counter++;
-    this.arr.push(this.counter);
-  }
 
 }
