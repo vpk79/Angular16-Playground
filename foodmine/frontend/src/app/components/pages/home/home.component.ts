d69e7f3fe7f0b42c033c199cdc10acc6e0ecb127
vpk79 <someone_bul@yahoo.com>
2024-04-28 23:47:46 +0300

    up

--- foodmine/frontend/src/app/components/pages/home/home.component.ts
+++ foodmine/frontend/src/app/components/pages/home/home.component.ts
@@ -16,7 +16,10 @@ export class HomeComponent {
     activatedRoute.params.subscribe((params) => {
       if (params.searchTerm) {
         this.foods = this.foodService.getAllFoodBySearchTerm(params.searchTerm);
-      } else {
+      } else if (params.tag) {
+        this.foods = this.foodService.getAllFoodsByTag(params.tag)
+      }
+      else {
         this.foods = foodService.getAll();
       }
     })
