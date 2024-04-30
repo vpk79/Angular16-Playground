81f462aa2ce5e8c309a874914e2b67e75a1bb3b2
vpk79 <someone_bul@yahoo.com>
2024-04-30 18:21:56 +0300

    up

--- foodmine/backend/src/server.ts
+++ foodmine/backend/src/server.ts
@@ -1,14 +1,16 @@
 import express from "express"
 import cors from "cors";
-import { sample_food, sample_tags } from "./data";
+import { sample_food, sample_tags, sample_users } from "./data";
+import jwt from "jsonwebtoken";
 
 const app = express();
+app.use(express.json());
 app.use(cors({
     credentials: true,
     origin: ["http://localhost:4200"]
 }))
 
-app.get("/api/foods",(req, res)=> {
+app.get("/api/foods", (req, res) => {
     res.send(sample_food);
 })
 
@@ -18,7 +20,7 @@ app.get("/api/foods/search/:searchTerm", (req, res) => {
     res.send(foods);
 })
 
-app.get("/api/foods/tags", (req, res)=> {
+app.get("/api/foods/tags", (req, res) => {
     res.send(sample_tags)
 })
 
@@ -34,7 +36,30 @@ app.get("/api/foods/:foodId", (req, res) => {
     res.send(food);
 })
 
+
+app.post("/api/users/login", (req, res) => {
+    const { email, password } = req.body;
+    const user = sample_users.find(user => user.email === email && user.password === password);
+
+    if (user) {
+        res.send(generateTokenResponse(user));
+    } else {
+        res.status(400).send("User name or password is not valid!")
+    }
+})
+
+const generateTokenResponse = (user: any) => {
+    const token = jwt.sign({
+        email: user.email, isAdmin: user.isAdmin
+    }, "SomeRandomText", {
+        expiresIn: "30d"
+    });
+
+    user.token = token;
+    return user;
+}
+
 const port = 5000;
-app.listen(port, ()=> {
-    console.log("Website served on http://localhost:"+ port);
+app.listen(port, () => {
+    console.log("Website served on http://localhost:" + port);
 })
