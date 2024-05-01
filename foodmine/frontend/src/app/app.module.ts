485b689e5f9cfa4fc6a1b2f30128da65b463dc98
vpk79 <someone_bul@yahoo.com>
2024-05-02 00:05:57 +0300

    up

--- foodmine/frontend/src/app/app.module.ts
+++ foodmine/frontend/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InputContainerComponent } from './components/partials/input-container/input-container.component';
 import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
 import { TextInputComponent } from './components/partials/text-input/text-input.component';
+import { DefaultButtonComponent } from './components/partials/default-button/default-button.component';
 
 
 @NgModule({
@@ -38,7 +39,8 @@ import { TextInputComponent } from './components/partials/text-input/text-input.
     LoginPageComponent,
     InputContainerComponent,
     InputValidationComponent,
-    TextInputComponent
+    TextInputComponent,
+    DefaultButtonComponent
   ],
   imports: [
     BrowserModule,
