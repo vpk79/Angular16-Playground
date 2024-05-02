3b8d6100e83d0d73c0a4535a55f2798c432b2955
vpk79 <someone_bul@yahoo.com>
2024-05-02 13:59:42 +0300

    up

---
+++ foodmine/backend/src/routers/food.router.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { sample_food, sample_tags } from "../data";
+
+
+const router = Router();
+
+
+router.get("/", (req, res) => {
+    res.send(sample_food);
+})
+
+router.get("/search/:searchTerm", (req, res) => {
+    const searchTerm = req.params.searchTerm;
+    const foods = sample_food.filter(food => food.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()));
+    res.send(foods);
+})
+
+router.get("/tags", (req, res) => {
+    res.send(sample_tags)
+})
+
+router.get("/tag/:tagName", (req, res) => {
+    const tagName = req.params.tagName;
+    const foods = sample_food.filter(food => food.tags?.includes(tagName));
+    res.send(foods);
+})
+
+router.get("/:foodId", (req, res) => {
+    const foodId = req.params.foodId;
+    const food = sample_food.find(food => food.id == foodId);
+    res.send(food);
+})
+
+export default router;
