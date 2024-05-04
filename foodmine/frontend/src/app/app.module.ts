e5e0535358a25599f168e49123fa08b4b5198c61
vpk79 <someone_bul@yahoo.com>
2024-05-05 00:32:27 +0300

    up

--- foodmine/frontend/src/app/app.module.ts
+++ foodmine/frontend/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { LoadingComponent } from './components/partials/loading/loading.componen
 import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
 import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
 import { OrderItemListComponent } from './components/partials/order-item-list/order-item-list.component';
+import { MapComponent } from './components/partials/map/map.component';
 
 
 @NgModule({
@@ -49,7 +50,8 @@ import { OrderItemListComponent } from './components/partials/order-item-list/or
     RegisterPageComponent,
     LoadingComponent,
     CheckoutPageComponent,
-    OrderItemListComponent
+    OrderItemListComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
---
+++ foodmine/frontend/src/app/components/partials/map/map.component.ts
@@ -0,0 +1,71 @@
+import { LocationService } from './../../../services/location.service';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { LatLng, LatLngExpression, LatLngTuple, LeafletEvent, LeafletMouseEvent, Map, Marker, icon, latLng, map, marker, tileLayer } from 'leaflet';
+
+@Component({
+  selector: 'map',
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements OnInit {
+
+  private readonly MARKER_ZOOM_LEVEL = 16;
+  private readonly MARKER_ICON = icon({
+    iconUrl:
+      'https://res.cloudinary.com/foodmine/image/upload/v1638842791/map/marker_kbua9q.png',
+    iconSize: [42, 42],
+    iconAnchor: [21, 42]
+  })
+  private readonly DEFAULT_LATLNG: LatLngTuple = [13.75, 21.62];
+
+  @ViewChild('map', { static: true })
+  mapRef!: ElementRef;
+  map!: Map;
+  currentMarker!: Marker;
+
+  constructor(private locationService: LocationService) { }
+
+  ngOnInit(): void {
+
+    this.initializeMap();
+  }
+
+  initializeMap() {
+
+    if (this.map) return;
+
+    this.map = map(this.mapRef.nativeElement, {
+      attributionControl: false
+    }).setView(this.DEFAULT_LATLNG, 1)
+
+    tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(this.map);
+
+    this.map.on('click', (e:LeafletMouseEvent) => {
+      this.setMarker(e.latlng);
+    })
+  }
+
+  findMyLocation() {
+    this.locationService.getCurrentLocation().subscribe({
+      next: (latLng) => {
+        this.map.setView(latLng, this.MARKER_ZOOM_LEVEL)
+       this.setMarker(latLng)
+
+      }
+    })
+  }
+
+  setMarker(latLng: LatLngExpression) {
+    if (this.currentMarker){
+      this.currentMarker.setLatLng(latLng)
+      return;
+    }
+
+    this.currentMarker = marker(latLng, {
+      draggable: true,
+      icon: this.MARKER_ICON
+    }).addTo(this.map);
+   
+  }
+
+}
