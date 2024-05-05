c08e0ed74a205ddd164e4f81f7b2b7e3ab010d28
vpk79 <someone_bul@yahoo.com>
2024-05-05 19:51:23 +0300

    up

---
+++ foodmine/backend/src/routers/order.router.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import  asyncHandler  from 'express-async-handler';
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+
+
+const router = Router();
+
+router.post('/create')
+asyncHandler(async(req:any, res:any) => {
+    const requestOrder = req.body;
+
+    if(requestOrder.items.length <= 0){
+        res.status(HTTP_BAD_REQUEST).send('Cart is Empty!');
+        return;
+    }
+})
+
