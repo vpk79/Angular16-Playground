a3cc40d49db100187a5ad6b7726e213c6821f53c
vpk79 <someone_bul@yahoo.com>
2024-05-06 23:53:07 +0300

    up

--- foodmine/frontend/src/app/app.module.ts
+++ foodmine/frontend/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { OrderItemListComponent } from './components/partials/order-item-list/or
 import { MapComponent } from './components/partials/map/map.component';
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { PaymentPageComponent } from './components/pages/payment-page/payment-page.component';
+import { PaypalButtonComponent } from './components/partials/paypal-button/paypal-button.component';
 
 
 @NgModule({
@@ -54,7 +55,8 @@ import { PaymentPageComponent } from './components/pages/payment-page/payment-pa
     CheckoutPageComponent,
     OrderItemListComponent,
     MapComponent,
-    PaymentPageComponent
+    PaymentPageComponent,
+    PaypalButtonComponent
   ],
   imports: [
     BrowserModule,
