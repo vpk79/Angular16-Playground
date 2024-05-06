77901a0397888d6682ce0f938e84348f82aba749
vpk79 <someone_bul@yahoo.com>
2024-05-07 00:07:58 +0300

    up

--- foodmine/frontend/src/app/services/order.service.ts
+++ foodmine/frontend/src/app/services/order.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Order } from '../shared/models/Order';
 import { HttpClient } from '@angular/common/http';
-import { ORDER_CREATE_URL, ORDER_NEW_FOR_CURRENT_USER_URL } from '../shared/constants/urls';
+import { ORDER_CREATE_URL, ORDER_NEW_FOR_CURRENT_USER_URL, ORDER_PAY_URL } from '../shared/constants/urls';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -17,6 +17,10 @@ export class OrderService {
 
   getNewOrderForCurrentUser(): Observable<Order>{
     return this.http.get<Order>(ORDER_NEW_FOR_CURRENT_USER_URL);
+  }
+
+ pay(order:Order): Observable<string> {
+    return this.http.post<string>(ORDER_PAY_URL, order);
   }
  
 }
