c952fe899fbd43ce62173eedcfcd865c632806ca
vpk79 <someone_bul@yahoo.com>
2024-05-06 19:43:59 +0300

    Update checkout-page.component.ts

--- foodmine/frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
+++ foodmine/frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
@@ -4,13 +4,15 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CartService } from '../../../services/cart.service';
 import { UserService } from '../../../services/user.service';
 import { ToastrService } from 'ngx-toastr';
+import { OrderService } from '../../../services/order.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout-page',
   templateUrl: './checkout-page.component.html',
   styleUrl: './checkout-page.component.css'
 })
-export class CheckoutPageComponent implements OnInit{
+export class CheckoutPageComponent implements OnInit {
   order: Order = new Order();
   checkoutForm!: FormGroup;
 
@@ -18,34 +20,47 @@ export class CheckoutPageComponent implements OnInit{
     cartService: CartService,
     private FormBuilder: FormBuilder,
     private userService: UserService,
-    private toastrService: ToastrService) {
+    private toastrService: ToastrService,
+    private orderService: OrderService,
+    private router: Router) {
     const cart = cartService.getCart();
     this.order.items = cart.items;
     this.order.totalPrice = cart.totalPrice;
   }
 
   ngOnInit(): void {
-    let {name, address} = this.userService.currentUser;
+    let { name, address } = this.userService.currentUser;
     this.checkoutForm = this.FormBuilder.group({
-      name:[name, Validators.required],
-      address:[address, Validators.required]
+      name: [name, Validators.required],
+      address: [address, Validators.required]
     })
   }
 
-  get fc(){
+  get fc() {
     return this.checkoutForm.controls;
   }
 
-  createOrder(){
-    if(this.checkoutForm.invalid){
-      this.toastrService.warning('Please fill the inputs', 'Invalid Inputs');
+  createOrder() {
+    if (this.checkoutForm.invalid) {
+      this.toastrService.warning('Please, fill the inputs', 'Invalid Inputs');
+      return;
+    }
+
+    if (!this.order.addressLatLng) {
+      this.toastrService.warning('Please, select your location on the map', 'Location');
       return;
     }
 
     this.order.name = this.fc.name.value;
     this.order.address = this.fc.address.value;
 
-    console.log(this.order);
-    
+    this.orderService.create(this.order).subscribe({
+      next:()=> {
+        this.router.navigateByUrl('/payment');
+      },
+      error:(errorResponse)=> {
+        this.toastrService.error(errorResponse.error, 'Cart');
+      }
+    })
   }
 }
